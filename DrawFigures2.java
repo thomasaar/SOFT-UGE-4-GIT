221e7ffc117e981cd6f51ff25371641d36b0c702
thomasaar <taar@live.dk>
2016-09-23 08:32:53 +0200

    Add files via upload

---
+++ DrawFigures2.java
@@ -0,0 +1,80 @@
+public class DrawFigures2 {
+	public static void main (String[] args) {
+		drawHeart();
+		drawBLANK();
+		drawtTOP();
+		drawtBUND();
+		drawBLANK();
+		drawtBUND();
+		drawBLANK();
+		drawL();
+		drawBLANK();
+		drawL();
+		drawBLANK();
+		drawY();
+		drawBLANK();
+		drawK();
+		drawBLANK();
+		drawK();
+		drawBLANK();
+		drawE();
+		drawBLANK();
+		drawHeart();
+		
+		
+	}
+
+	public static void drawtTOP() {
+		System.out.println(" ______");
+	}
+	
+	public static void drawtBUND() {
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+	}
+	
+	public static void drawBLANK() {
+		System.out.println("      ");
+		System.out.println("      ");
+		
+	}
+	
+	public static void drawL() {
+		System.out.println("   I   ");
+		System.out.println("   I__   ");
+		
+	}
+	
+	public static void drawY() {
+		System.out.println("  \\ /  ");
+		System.out.println("   V  ");
+		System.out.println("   I   ");
+		
+	}
+	
+	public static void drawK() {
+		System.out.println("   I/   ");
+		System.out.println("   I\\  ");
+		
+	}
+	
+	public static void drawE() {
+		System.out.println("    _   ");
+		System.out.println("   I_   ");
+		System.out.println("   I_   ");
+		
+	}
+	
+	public static void drawHeart() {
+		System.out.println("   _    _   ");
+		System.out.println(" (   \\/   ) ");
+		System.out.println("  \\      /  ");
+		System.out.println("   \\    /   ");
+		System.out.println("    \\  /    ");
+		System.out.println("     \\/     ");
+		
+	}
+	
+	
+}	
